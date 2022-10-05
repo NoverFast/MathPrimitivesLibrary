4edc739260beb073f04c8f0367c3fceb55a6ee0a
vladislav.afletunov <vladislav.afletunov@WS26>
2022-10-05 12:22:07 +0500

    Добавил модуль со статистикой. Добавил забытые ранее регионы в класс матрицы.

--- MathPrimitivesLibrary/Statistics/Statistics.cs
+++ MathPrimitivesLibrary/Statistics/Statistics.cs
@@ -6,8 +6,43 @@ using System.Threading.Tasks;
 
 namespace MathPrimitivesLibrary.Statistics
 {
-  public class Statistics
+  public static class Statistics
   {
+    public static double Mean(double[] data)
+    {
+      double sum = 0;
+      for (int i = 0; i < data.Length; i++)
+      {
+        sum += data[i];
+      }
+      return sum / data.Length;
+    }
 
+    public static double MeanAbsoluteDeviation(double[] data)
+    {
+      double sum = 0;
+      double mean = Mean(data);
+      for (int i =0; i < data.Length; i++)
+      {
+        sum += Math.Abs(data[i] - mean);
+      }
+      return sum / data.Length;
+    }
+
+    public static double Dispersion(double[] data)
+    {
+      double sum = 0;
+      double expectancy = Mean(data);
+      for (int i = 0; i < data.Length; i++)
+      {
+        sum += Math.Pow((data[i] - expectancy), 2);
+      }
+      return sum;
+    }
+
+    public static double CorrectedDispersion(double[] data)
+    {
+      return Dispersion(data) * (double)(1.0 / (data.Length - 1));
+    }
   }
 }
