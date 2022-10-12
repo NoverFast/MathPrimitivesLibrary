6aed9a38ba894d2a83f4debb5de772759b8cf1ff
vladislav.afletunov <vladislav.afletunov@WS26>
2022-10-12 15:13:57 +0500

    Документация для класса статистики

--- MathPrimitivesLibrary/Statistics/Statistics.cs
+++ MathPrimitivesLibrary/Statistics/Statistics.cs
@@ -8,6 +8,10 @@ namespace MathPrimitivesLibrary.Statistics
 {
   public static class Statistics
   {
+    /// <summary>
+    /// Calculates mean of provided sample. 
+    /// </summary>
+    /// <param name="data">Sample</param>
     public static double Mean(double[] data)
     {
       double sum = 0;
@@ -18,6 +22,10 @@ namespace MathPrimitivesLibrary.Statistics
       return sum / data.Length;
     }
 
+    /// <summary>
+    /// Calculates MAD (Mean Absolute Deviation) of provided sample. 
+    /// </summary>
+    /// <param name="data">Sample</param>
     public static double MeanAbsoluteDeviation(double[] data)
     {
       double sum = 0;
@@ -29,6 +37,10 @@ namespace MathPrimitivesLibrary.Statistics
       return sum / data.Length;
     }
 
+    /// <summary>
+    /// Calculates dispersion of provided sample. 
+    /// </summary>
+    /// <param name="data">Sample</param>
     public static double Dispersion(double[] data)
     {
       double sum = 0;
@@ -40,12 +52,39 @@ namespace MathPrimitivesLibrary.Statistics
       return sum / data.Length;
     }
 
+    /// <summary>
+    /// Calculates corrected dispersion of provided sample. 
+    /// </summary>
+    /// <param name="data">Sample</param>
+    /// <returns>Dispersion multiplied by (n / (n-1)) where n is the size of the sample.</returns>
     public static double CorrectedDispersion(double[] data)
     {
       return Dispersion(data) * ((double)data.Length / (double)(data.Length - 1));
     }
 
-    public static Matrix NormalizeData(Matrix data)
+    /// <summary>
+    /// Normalizes provided sample by substracting mean and dividing by square root of dispersion. 
+    /// </summary>
+    /// <param name="data">Sample</param>
+    /// <returns>Normalized sample.</returns>
+    public static Vector NormalizeData(double[] data)
+    {
+      Vector normalizedData = new Vector(data.Length);
+      double mean = Mean(normalizedData.ToArray());
+      double dispersion = Dispersion(normalizedData.ToArray());
+      for (int i = 0; i < normalizedData.Size; i++)
+      {
+        normalizedData[i] = Math.Round((data[i] - mean) / Math.Sqrt(dispersion), 2);
+      }
+      return normalizedData;
+    }
+
+    /// <summary>
+    /// Normalizes provided samples by substracting mean and dividing by square root of dispersion. 
+    /// </summary>
+    /// <param name="data">Sample</param>
+    /// <returns>Normalized matrix of samples.</returns>
+    public static Matrix NormalizeData(double[,] data)
     {
       Matrix normalizedMatrix = new Matrix(data);
       double mean;
@@ -62,19 +101,25 @@ namespace MathPrimitivesLibrary.Statistics
       return normalizedMatrix;
     }
 
-    public static Matrix CorrelationMatrix(Matrix normalizedMatrix)
+
+    /// <summary>
+    /// Build correlation matrix based on provided normalized matrix
+    /// </summary>
+    /// <param name="normalizedMatrix">Matrix with normalized samples</param>
+    /// <returns>Correlation matrix.</returns>
+    public static Matrix CorrelationMatrix(double[,] normalizedMatrix)
     {
-      Matrix correlationMatrix = new Matrix(normalizedMatrix.Rows);
+      Matrix correlationMatrix = new Matrix(normalizedMatrix.GetLength(0));
       double sum = 0;
-      for (int i = 0; i < normalizedMatrix.Rows; i++)
+      for (int i = 0; i < normalizedMatrix.GetLength(0); i++)
       {
-        for (int j = 0; j < normalizedMatrix.Rows; j++)
+        for (int j = 0; j < normalizedMatrix.GetLength(0); j++)
         {
-          for (int k = 0; k < normalizedMatrix.Coloumns; k++)
+          for (int k = 0; k < normalizedMatrix.GetLength(1); k++)
           {
             sum += normalizedMatrix[i, k] * normalizedMatrix[j, k];
           }
-          correlationMatrix[i, j] = Math.Round(sum / normalizedMatrix.Coloumns, 2);
+          correlationMatrix[i, j] = Math.Round(sum / normalizedMatrix.GetLength(1), 2);
           sum = 0;
           if (i == j)
           {
@@ -85,6 +130,12 @@ namespace MathPrimitivesLibrary.Statistics
       return correlationMatrix;
     }
 
+    /// <summary>
+    /// Builsd linear regression equation between two samples of data
+    /// </summary>
+    /// <param name="data1">First sample</param>
+    /// <param name="data2">Second sample</param>
+    /// <returns>Vector of 1-st degree polynomial coefficients of linear regression equation.</returns>
     public static Vector LinearRegression(double[] data1, double[] data2)
     {
       if (data1.SequenceEqual(data2))
@@ -95,12 +146,11 @@ namespace MathPrimitivesLibrary.Statistics
       double d2Mean = Mean(data2);
       double d1Deviation = Math.Sqrt(Dispersion(data1));
       double d2Deviation = Math.Sqrt(Dispersion(data2));
-      Matrix dataMatrix = NormalizeData(new Matrix(new double[][] { data1, data2 }));
-      Matrix correlationMatrix = CorrelationMatrix(dataMatrix);
+      Matrix dataMatrix = NormalizeData(new Matrix(new double[][] { data1, data2 }).To2DArray());
+      Matrix correlationMatrix = CorrelationMatrix(dataMatrix.To2DArray());
       return new Vector(new double[] {
         Math.Round(d1Mean - correlationMatrix[0, 1] * d2Mean * d1Deviation / d2Deviation, 2),
         Math.Round(correlationMatrix[0,1] * d1Deviation / d2Deviation, 2) });
     }
-
   }
 }
