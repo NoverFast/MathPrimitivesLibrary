4f134d4021618e51e098f266dafc5c84fc27948f
Ismagil <ismagil-2001@mail.ru>
2022-10-28 18:23:25 +0500

    Добавил классы сетки и классы решателей ОДУ. Реализовал метод Эйлера.

---
+++ MathPrimitivesLibrary/Types/LinearODE/AbstractODESolver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MathPrimitivesLibrary.Types.Meshes;
+
+namespace MathPrimitivesLibrary.Types.LinearODE
+{
+  public class AbstractODESolver 
+  {
+    protected Mesh mesh { get; set; }
+    /// <summary>
+    /// При вызове конструктора происходит создание сетки и инициализация первого элемента сетки начальными данными.
+    /// </summary>
+    /// <param name="leftEdge"> Левая граница расчёта </param>
+    /// <param name="rightEdge"> Правая граница расчёта </param>
+    /// <param name="steps"> Кол-во шагов на сетке </param>
+    /// <param name="function"> Исходная задача </param>
+    
+    public AbstractODESolver(double leftEdge, double rightEdge, int numberOfSteps, Func<double, double> function)
+    {
+      mesh = new Mesh(leftEdge, rightEdge, numberOfSteps);
+    }
+  }
+}
---
+++ MathPrimitivesLibrary/Types/LinearODE/Euler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathPrimitivesLibrary.Types.LinearODE
+{
+  public class Euler : AbstractODESolver
+  {
+    /// <summary>
+    /// Начальное условие задачи Коши
+    /// </summary>
+    private double y0 { get; set; }
+    private double x0 { get; set; }
+    private Func<double, double> function { get; set; }
+    /// <summary>
+    /// Метод Эйлера является одношаговым, следовательно при инициализации сетки в ней уже будет как минимум одно значение y(leftEdge) (Начальные Условия)
+    /// </summary>
+    /// <param name="leftEdge"></param>
+    /// <param name="rightEdge"></param>
+    /// <param name="numberOfSteps"></param>
+    /// <param name="function"></param>
+    public Euler(double leftEdge, double rightEdge, int numberOfSteps, Func<double, double> function) : base(leftEdge, rightEdge, numberOfSteps, function)
+    {
+      this.function = function;
+      x0 = mesh.MeshSteps[0] = leftEdge;
+      y0 = mesh.MeshValue[0] = this.function(leftEdge);
+    }
+    public void SolveODE()
+    {
+      for (int i = 1; i <= mesh.NumberOfSteps; i++)
+      {
+        mesh.MeshSteps[i] += mesh.StepLength;
+        mesh.MeshValue[i] = mesh.MeshValue[i - 1] + mesh.StepLength * function(mesh.MeshSteps[i]);
+      }
+#if DEBUG
+      mesh.GetMeshData();
+#endif
+    }
+  }
+}
