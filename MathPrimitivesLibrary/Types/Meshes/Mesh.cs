c22707aa1eb897ba05d696bb95d36fa81e5ade6b
Ismagil <ismagil-2001@mail.ru>
2022-10-28 18:23:25 +0500

    Добавил классы сетки и классы решателей ОДУ. Реализовал метод Эйлера.

---
+++ MathPrimitivesLibrary/Types/Meshes/Mesh.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MathPrimitivesLibrary.Types.Meshes
+{
+  public class Mesh
+  {
+    public int NumberOfSteps { get; set; }
+    public double LeftEdge { get; set; }
+    public double RightEdge { get; set; }
+    public double StepLength { get; set; }
+    public double[] MeshValue { get; set; }
+    public double[] MeshSteps { get; set; }
+    public Mesh(double leftEdge, double rightEdge, int numberOfSteps)
+    {
+      this.LeftEdge = leftEdge;
+      this.RightEdge = rightEdge;
+      this.NumberOfSteps = numberOfSteps;
+      StepLength = (this.RightEdge - this.LeftEdge) / this.NumberOfSteps;
+      MeshValue = new double[this.NumberOfSteps + 1];
+      MeshSteps = new double[this.NumberOfSteps + 1];
+#if VERBOSE
+        GetMeshProperties();
+#endif
+    }
+
+    private void GetMeshProperties()
+    {
+      Console.WriteLine($"Left Edge: {this.LeftEdge}");
+      Console.WriteLine($"Right Edge: {this.RightEdge}");
+      Console.WriteLine($"Number of Steps: {this.NumberOfSteps}");
+      Console.WriteLine($"Step Length: {this.StepLength}");
+    }
+
+    public void GetMeshData()
+    {
+      Console.WriteLine("y: ");
+      foreach (double v in MeshValue)
+      {
+        Console.Write($"{v}\t");
+      }
+      Console.WriteLine("x: ");
+      foreach (double s in MeshSteps)
+      {
+        Console.Write($"{s}\t");
+      }
+    }
+  }
+}
