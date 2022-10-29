27598c2ad4b3e3e2267638e0dcb8f73d85c3ed87
vladislav.afletunov <vladislav.afletunov@WS26>
2022-10-29 14:37:40 +0500

    Начал писать квадратурные формулы

--- MathPrimitivesLibrary/Types/Quadratures/AbstractCompositionalQuadrature.cs
+++ MathPrimitivesLibrary/Types/Quadratures/AbstractCompositionalQuadrature.cs
@@ -1,10 +1,21 @@
 ﻿using MathPrimitivesLibrary.Types.Meshes;
+using System;
 
 namespace MathPrimitivesLibrary.Types.Quadratures
 {
   abstract class AbstractCompositionalQuadrature
   {
     protected RegularMesh mesh { get; set; }
-    public void AbstractCompositionalQuadrature(double leftEdge, double rightEdge, RegularMesh mesh)
+    protected Func<double, double> function { get; set; }
+    public AbstractCompositionalQuadrature(RegularMesh mesh, Func<double, double> function)
+    {
+      this.mesh = mesh;
+      this.function = function;
+      this.mesh.MeshX[0] = mesh.LeftEdge;
+      this.mesh.MeshY[0] = function(mesh.LeftEdge);
+    }
+    
+    public virtual void Solve()
+    { }
   }
 }
--- MathPrimitivesLibrary/Types/Quadratures/Rectangle.cs
+++ MathPrimitivesLibrary/Types/Quadratures/Rectangle.cs
@@ -3,8 +3,40 @@ using MathPrimitivesLibrary.Types.Meshes;
 
 namespace MathPrimitivesLibrary.Types.Quadratures
 {
-  class Rectangle
+  class Rectangle : AbstractCompositionalQuadrature
   {
-    
+    private bool useCentral { get; }
+    public Rectangle(RegularMesh mesh, Func<double, double> function, bool useCentral) : base(mesh, function)
+    {
+      this.useCentral = useCentral;
+    }
+
+    public override void Solve()
+    {
+      if (useCentral)
+      {
+
+      }
+      else
+      {
+        RightLeft();
+      }
+    }
+
+    private void Middle()
+    {
+      double sum = 0;
+    }
+
+    private void RightLeft()
+    {
+      double sum = 0;
+      for (int i = 1; i < mesh.NumberOfSteps; i++)
+      {
+        sum += function(mesh.MeshX[i]) * mesh.StepLength;
+        mesh.MeshX[i] = mesh.StepLength * i;
+        mesh.MeshY[i] = sum;
+      }
+    }
   }
 }
