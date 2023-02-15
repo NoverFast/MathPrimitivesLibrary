8aa1d5478dc544a450c5e4cb39aa1365862b7d41
Nover <onverx@gmail.com>
2023-02-15 12:06:39 +0500

    Изменил методы двумерной сетки под тип Point

--- MathPrimitivesLibrary/Types/Meshes/RegularMesh2D.cs
+++ MathPrimitivesLibrary/Types/Meshes/RegularMesh2D.cs
@@ -2,14 +2,27 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace MathPrimitivesLibrary.Types.Meshes
 {
+
   public class RegularMesh2D : AbstractMesh
   {
+    public struct Point2D
+    {
+      public double X;
+      public double Y;
+
+      public void Show()
+      {
+        Console.WriteLine($"X: {X}, Y: {Y}");
+      }
+    }
+
     #region Properties
     /// <summary>
     /// Количество шагов по оси X
@@ -37,21 +50,19 @@ namespace MathPrimitivesLibrary.Types.Meshes
     /// <summary>
     /// Матрица, которая содержит в себе все числовые значения точек в сетке
     /// </summary>
-    public Matrix GridPointsX { get; set; }
-    public Matrix GridPointsY { get; set; }
+    public Point2D[,] GridPoints { get; private set; }
+
+
     #endregion
 
     #region Constructors
     public RegularMesh2D() : base(0, 1, 100)
     {
       Grid = new Matrix(NumberOfStepsX, NumberOfStepsY);
-      GridPointsX = new Matrix(NumberOfStepsX, NumberOfStepsY);
-      GridPointsY = new Matrix(NumberOfStepsX, NumberOfStepsY);
+      GridPoints = new Point2D[NumberOfStepsX, NumberOfStepsY];
       NumberOfStepsX = base.numberOfSteps;
       NumberOfStepsY = base.numberOfSteps;
       StepLengthX = StepLengthY = (base.rightEdge - base.leftEdge) / (base.numberOfSteps -1);
-
-      FillGridPoints();
     }
 
     public RegularMesh2D(double[] leftBottom, double[] rightBottom, 
@@ -59,30 +70,16 @@ namespace MathPrimitivesLibrary.Types.Meshes
       base(leftBottom[0], rightBottom[0], numberOfStepsX)
     {
       Grid = new Matrix(numberOfStepsX, numberOfStepsY);
-      GridPointsX = new Matrix(numberOfStepsX, numberOfStepsY);
-      GridPointsY = new Matrix(numberOfStepsX, numberOfStepsY);
+      GridPoints = new Point2D[NumberOfStepsX, NumberOfStepsY];
       NumberOfStepsX = numberOfStepsX;
       NumberOfStepsY = numberOfStepsY;
       StepLengthX = Math.Abs(rightTop[0] - leftTop[0]) / (numberOfStepsX - 1);
       StepLengthY = Math.Abs(rightTop[1] - leftBottom[1]) / (numberOfStepsY - 1);
 
-      FillGridPoints();
+      FillGridPoints(leftTop);
     }
 
     #endregion
-
-    // TODO переписать под нормальный дженерик
-    public void WriteMeshDataToFile(string path)
-    {
-      StreamWriter sw = new StreamWriter(path);
-      for (int i = 0; i < this.NumberOfStepsX; i++)
-      {
-        for (int j = 0; j < this.NumberOfStepsY; j++)
-        {
-          sw.WriteLine(GridPointsX[i, j] + "  " + GridPointsY[i,j] + " " + Grid[i, j]);
-        }
-      }
-    }
 
     public override void ShowMeshProperties(bool showGrid = false, int roundTo = -1)
     {
@@ -90,36 +87,35 @@ namespace MathPrimitivesLibrary.Types.Meshes
       Console.WriteLine($"Step Length by X: {StepLengthX}\nStep Length by Y: {StepLengthY}");
       if (showGrid)
       {
-        GridPointsX.Show(roundTo);
-        GridPointsY.Show(roundTo);
+        foreach (Point2D p in GridPoints)
+        {
+          p.Show();
+        }
       }
     }
 
     public override void ClearMeshData()
     {
-      for (int i =0; i < GridPointsX.Rows; i++)
+      for (int i =0; i < GridPoints.GetLength(0); i++)
       {
-        for (int j =0; j < GridPointsX.Coloumns; j++)
+        for (int j =0; j < GridPoints.GetLength(1); j++)
         {
           Grid[i, j] = 0;
         }
       }
     }
 
-    private void FillGridPoints()
+    private void FillGridPoints(double[] leftUpperCorner)
     {
-      double tmpX = -1;
-      double tmpY = -1;
-      for (int i = 0; i < GridPointsX.Rows; i++)
+      for (int i = 0; i < GridPoints.GetLength(0); i++)
       {
-        for (int j = 0; j < GridPointsX.Coloumns; j++)
+        for (int j = 0; j < GridPoints.GetLength(1); j++)
         {
-          tmpY += StepLengthY * j;
-          GridPointsX[i, j] = tmpX;
-          GridPointsY[i, j] = tmpY;
+          GridPoints[i,j]= new Point2D() 
+          { 
+            X = leftUpperCorner[0] + j * StepLengthX, Y = leftUpperCorner[1] - i * StepLengthY 
+          };
         }
-        tmpX += StepLengthX * i;
-        tmpY = StepLengthY;
       }
     }
   }
